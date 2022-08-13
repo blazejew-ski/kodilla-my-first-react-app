2f35c5738a58e8a2a8c71d7b79be6ce43ae91830
Marcin Błażejewski <macin@blazejew.ski>
2022-08-13 10:59:52 +0200

    new column adding mechanism added with title and icon support

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,7 +4,7 @@ import styles from './Column.module.scss'
 const Column = props => {
   return (
     <article className={styles.column}>
-      <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.text}</h3>
+      <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h3>
     </article>
   );
 };
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,28 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
+import ColumnForm from '../ColumnForm/ColumnForm.js';
+import { useState } from 'react';
+import shortid from 'shortid';
+
 
 const List = () => {
+
+
+  const [columns, setColumns] = useState([
+    { id: 1, title: 'Books', icon: 'book' },
+    { id: 2, title: 'Movies', icon: 'film' },
+    { id: 3, title: 'Games', icon: 'gamepad' }
+  ]);
+
+  const addColumn = newColumn => {
+		setColumns([...columns, {
+      id: shortid(),
+      title: newColumn.title,
+      icon: newColumn.icon
+    }]);
+  };
+
+
   return (
     <div>
         <header className={styles.header}>
@@ -9,10 +30,10 @@ const List = () => {
         </header>
         <p className={styles.description}>Interesting things I want to check out</p>
         <section className={styles.columns}>
-          <Column text="Books" icon="book"/>
-          <Column text="Movies" icon="film" />
-          <Column text="Games" icon="gamepad" />
+          {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
+          <ColumnForm action={addColumn} />
         </section>
+
     </div>
   );
 };
